c9b921d46ef5ffa8f51a04f37f249621f9464d72
Andrew Gulik <andrewgulik@gmail.com>
2018-11-02 21:23:16 -0400

    sending deal + link

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 const Xray = require("x-ray");
+const Telegraf = require("telegraf");
 
 const x = Xray();
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.startPolling();
 
 const crawl = async () => {
   console.log("crawling...");
-  const response = await x(
-    "https://www.secretflying.com/search2/?cityFrom=Montreal&cityTo=&month=",
-    "article",
-    [
-      {
-        deal: "h2 a"
-      }
-    ]
+  const [response] = await x(process.env.WEB_URL, "article", [
+    {
+      deal: "h2 a",
+      link: "h2 a@href"
+    }
+  ]);
+  bot.telegram.sendMessage(
+    process.env.CHAT_ID,
+    `New Flight Deal! ✈️ - ${response.deal} click here: ${response.link}`
   );
   console.log(response);
 };
